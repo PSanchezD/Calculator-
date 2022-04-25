2df6b3b7c8911be59d662307b175713822f404ad
pedro sanchez <pedrosanchez707@gmail.com>
2022-04-25 16:30:55 -0700

    CSS/UI update

--- src/components/main.js
+++ src/components/main.js
@@ -22,32 +22,41 @@ function Main(){
 
         const equal =()=>{
             setResult(evaluate(result))
-        }
+        };
+    const back =()=>{
+        setResult(result.slice(0,-1))
+    }
 
     return(
         <>
+            <div className="jumbo">
+                <h1>Calculator</h1>
+            </div>
            <div className="container">
                <div className="cal-c">
-                   <h1>{result}</h1>
+                   <div className="result-c">
+                    <h1>{result}</h1>
+                   </div>
                    <div className="numb-c">
-                    <button onClick={handleOne}>1</button>
-                    <button onClick={handleOne}>2</button>
-                    <button onClick={handleOne}>3</button>
-                    <button onClick={handleOne}>4</button>
-                    <button onClick={handleOne}>5</button>
-                    <button onClick={handleOne}>6</button>
-                    <button onClick={handleOne}>7</button>
-                    <button onClick={handleOne}>8</button>
-                    <button onClick={handleOne}>9</button>
-                    <button onClick={handleOne}>0</button>
+                        <button onClick={handleOne}>1</button>
+                        <button onClick={handleOne}>2</button>
+                        <button onClick={handleOne}>3</button>
+                        <button onClick={handleOne}>4</button>
+                        <button onClick={handleOne}>5</button>
+                        <button onClick={handleOne}>6</button>
+                        <button onClick={handleOne}>7</button>
+                        <button onClick={handleOne}>8</button>
+                        <button onClick={handleOne}>9</button>
+                        <button onClick={handleOne}>0</button>
+                        <button onClick={back}>C</button>
+                        <button onClick={clear}>AC</button>
                    </div>
                    <div className="opt-c">
-                    <button onClick={operation}>+</button>
-                    <button onClick={operation}>-</button>
-                    <button onClick={operation}>*</button>
-                    <button onClick={operation}>/</button>
-                    <button onClick={equal}>=</button>
-                    <button onClick={clear}>AC</button>
+                        <button onClick={operation}>+</button>
+                        <button onClick={operation}>-</button>
+                        <button onClick={operation}>*</button>
+                        <button onClick={operation}>/</button>
+                        <button onClick={equal}>=</button>
                    </div>
                </div>
            </div>
